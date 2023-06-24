57a456798cd39f270c935370251129c4ee5aa782
Nathan Whitehead <nwhitehe@gmail.com>
2023-06-24 14:17:32 -0600

    Make tiktoken counter demo.

--- src/Demo.vue
+++ src/Demo.vue
@@ -1,25 +1,43 @@
+
 <script setup>
+
+import { ref, computed } from 'vue';
+import { get_encoding, encoding_for_model } from "@dqbd/tiktoken";
+
+const encoder = get_encoding("gpt2");
+
+const inputText = ref("");
+
+const stats = computed(() => {
+    return {
+        characters: inputText.value.length,
+        tokens: encoder.encode(inputText.value).length,
+    };
+});
 
 </script>
 
 <template>
 
-<section>
-    <div class="container mx-auto max-w-screen-xl flex flex-col items-center justify-center px-16">
+    <section>
+        <div class="container mx-auto max-w-screen-xl flex flex-col items-center justify-center px-16">
 
-        <div class="text-4xl md:text-5xl font-display font-semibold text-blue-950">
-            <p class="text-center leading-relaxed">
-                <span class="text-pink-700">Create personalities</span> that can chat and play
-            </p>
-            <p class="text-center leading-relaxed text-3xl">
-                Cutting edge conversational and emotion technology
-            </p>
+            <div class="text-4xl md:text-5xl font-display font-semibold text-blue-950">
+                <p class="text-center leading-relaxed">
+                    Token Counter
+                </p>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<section>
-<p>Content</p>
-</section>
+    <section>
+        <p>Enter text here:</p>
+        <textarea rows="10" cols="50" v-model="inputText">
+        </textarea>
+        <p>
+        <br>Characters: {{ stats.characters }}
+        <br>Tokens: {{ stats.tokens }}
+        </p>
+    </section>
 
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 
 import { defineConfig } from 'vite';
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [ vue(), ],
+    plugins: [ wasm(), topLevelAwait(), vue(), ],
 })
