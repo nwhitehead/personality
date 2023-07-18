dc31d77da8f582382f037408d92312e5f6d8c149
Nathan Whitehead <nwhitehe@gmail.com>
2023-07-18 15:01:16 -0600

    Move embedder python file to rust. Write rust demo to show working with python module.

--- rustserver/src/main.rs
+++ rustserver/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use pyo3::prelude::*;
+
+fn main() -> PyResult<()> {
+    let py_embedder = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/embedder.py"));
+    Python::with_gil(|py| {
+        PyModule::from_code(py, py_embedder, "embedder", "embedder")?;
+        let embedder_module = py.import("embedder")?;
+        let embedder_class: Py<PyAny> = embedder_module.getattr("Embedder")?.into();
+        let embedder: Py<PyAny> = embedder_class.call1(py, ("intfloat/e5-base-v2", ))?.into();
+
+        println!("Object: {}", embedder);
+        let res: Py<PyAny> = embedder
+            .call_method1(py, "embed", ([
+                "What is your eye color?",
+                "What is your favorite classic Hollywood flick?",
+                "Is 8 a perfect number?",
+                "Do you like painting?",
+            ], "query: "))?
+            .call_method0(py, "cpu")?
+            .call_method0(py, "detach")?
+            .call_method0(py, "numpy")?
+            .call_method0(py, "tolist")?
+            .into();
+        println!("Results: {}", res);
+        Ok(())
+    })
 }
