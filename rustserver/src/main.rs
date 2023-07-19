b3cb802a0caa6508f69d1d4425d0d222562aedcd
Nathan Whitehead <nwhitehe@gmail.com>
2023-07-19 15:14:28 -0600

    Add hashing, printing some stuff, pickledb.

--- rustserver/src/main.rs
+++ rustserver/src/main.rs
@@ -2,30 +2,43 @@ use pyo3::prelude::*;
 use std::vec::Vec;
 use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
 use serde::{Deserialize, Serialize};
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
 
 fn main() -> PyResult<()> {
-    let mut db = PickleDb::new("test.db", PickleDbDumpPolicy::AutoDump, SerializationMethod::Json);
+    let embedding_cache_filename = std::env::var("EMBEDDING_CACHE")
+    .expect("EMBEDDING_CACHE must be set to writable filename location");
+    let mut db = PickleDb::new(embedding_cache_filename, PickleDbDumpPolicy::DumpUponRequest, SerializationMethod::Json);
     let py_embedder = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/embedder.py"));
+    let data = vec![
+        "What is your eye color?",
+        "What is your favorite classic Hollywood flick?",
+        "Is 8 a perfect number?",
+        "Do you like painting?",
+    ];
     Python::with_gil(|py| {
         PyModule::from_code(py, py_embedder, "embedder", "embedder")?;
         let embedder_module = py.import("embedder")?;
         let embedder_class: Py<PyAny> = embedder_module.getattr("Embedder")?.into();
         let embedder: Py<PyAny> = embedder_class.call1(py, ("intfloat/e5-base-v2", ))?.into();
 
-        println!("Object: {}", embedder);
         let res = embedder
-            .call_method1(py, "embed", ([
-                "What is your eye color?",
-                "What is your favorite classic Hollywood flick?",
-                "Is 8 a perfect number?",
-                "Do you like painting?",
-            ], "query: "))?
+            .call_method1(py, "embed", (data.clone(), "query: "))?
             .call_method0(py, "cpu")?
             .call_method0(py, "detach")?
             .call_method0(py, "numpy")?
             .call_method0(py, "tolist")?;
         let res2: Vec<Vec<f64>> = res.extract(py)?;
-        println!("Results: {:#?}", res2);
+        for i in 1..res2.len() {
+            let h = calculate_hash(&data[i]);
+            println!("result {}: {} => {:#x} {:.3} {:.3} ... {:.3}", i, &data[i], h, res2[i][0], res2[i][1], res2[i][767]);
+        }
         Ok(())
     })
 }
