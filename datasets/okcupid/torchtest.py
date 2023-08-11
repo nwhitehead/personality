ebbfee649ffa7b591b71a5202fcda11420e28c94
Nathan Whitehead <nwhitehe@gmail.com>
2023-08-10 18:03:33 -0600

    Classify dataset.

--- datasets/okcupid/torchtest.py
+++ datasets/okcupid/torchtest.py
@@ -1,32 +1,63 @@
+
 import torch
 import random
+
+# Fixed seed for testing purposes
+random.seed(5)
+torch.manual_seed(5)
 
 # Setup where things are stored
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Fixed seed for testing purposes
-random.seed(5)
+class CustomDataset(torch.utils.data.Dataset):
+    """Custom synthetic testing dataset"""
+    def __init__(self, N, Q, C, similarity, samples):
+        """
+        N: number of simulated user responses
+        Q: number of questions
+        C: number of categories per question
+        similarity: chance any later answers copy first answer
+        samples: number of generated samples
+        """
+        self.N = N
+        self.Q = Q
+        self.C = C
+        self.similarity = similarity
+        self.samples = samples
+        # Actually generate the choices here
+        self.x = torch.randint(C, (N, Q))
+        # Now fill in same choices based on similarity (before we one-hot encode for simplicity)
+        for n in range(N):
+            for q in range(Q):
+                if torch.rand(1, 1) < similarity:
+                    self.x[n][q] = self.x[n][0]
+        # Convert to one-hot, flatten questions together
+        # Make numerically floating point
+        self.x = torch.flatten(torch.nn.functional.one_hot(self.x), 1) * 1.0
+        # Now fill in sample choices
+        self.sample_choices = torch.randint(N, (1, samples))
+        self.question_choices = torch.randint(C, (1, samples))
+
+    def __len__(self):
+        return self.samples
+    
+    def __getitem__(self, idx):
+        C = self.C
+        Q = self.Q
+        x = self.x
+        n = self.sample_choices[0][idx]
+        q = self.question_choices[0][idx]
+        i = x[n].clone()
+        o = x[n].clone()
+        mask = torch.tensor([0] * (Q * C))
+        i[q * C : q * C + C] = 0
+        mask[q * C : q * C + C] = 1
+        return i, o, mask
 
 # Generate some random data
-# N: number of samples
-N = 10
-# Q: number of questions
-Q = 3
-# C: number of categories per question
-C = 4
-# Generate integers for which answer picked
-x = torch.randint(C, (N, Q))
-# Now make later answers sometimes match first answer
-similarity = 0.5
-# How many samples to have in full dataset
-SAMPLES = N
-for n in range(N):
-    for q in range(Q):
-        if torch.rand(1, 1) < similarity:
-            x[n][q] = x[n][0]
-# Convert to one-hot, flatten questions together
-# Make numerically floating point
-x = torch.flatten(torch.nn.functional.one_hot(x), 1) * 1.0
+data = CustomDataset(10, 3, 4, 0.5, 20)
+for i in data:
+    print(i)
 
 # Let's try deleting one answer, train to get answer back
 def samples():
@@ -46,5 +77,5 @@ def criterion(predicted, target, mask):
     print(target * mask, predicted * mask)
     return mseloss(predicted * mask, target * mask)
 
-for (i, mask, o) in samples():
-    print((i, mask, o, criterion(i, o, mask)))
+# for (i, mask, o) in samples():
+#     print((i, mask, o, criterion(i, o, mask)))
