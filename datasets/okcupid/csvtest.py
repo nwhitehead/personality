84810cee6ffd3516ec06e852eacdc13b24faf196
Nathan Whitehead <nwhitehe@gmail.com>
2023-08-13 09:41:12 -0600

    Convert strings to integers in dataset.

--- datasets/okcupid/csvtest.py
+++ datasets/okcupid/csvtest.py
@@ -5,19 +5,23 @@ with open('./parsed_data_public_t.csv') as csvfile:
     next(reader)
     # Remember answer set sizes
     anssize = []
+    dataset = []
     # Look at all questions
     for row in reader:
         # Extract the set of possible answers
-        ans = set(row[1:])
-        if '' in ans:
-            ans.remove('')
+        ans = sorted(list(set(row[1:])))
         # Skip the question if the set of answers is not in reasonable range
         # This is the case for some "summary statistic" data that is included
         # Also ethnicity/country/etc. have many many choices
         if not(len(ans) > 1 and len(ans) < 5):
             continue
         anssize.append(len(ans))
+        d = {}
+        for ind, a in enumerate(ans):
+            d[a] = ind
+        answers = [d[x] for x in row[1:]]
+        dataset.append(answers)
+        print(len(dataset))
     print(anssize)
     print(len(anssize))
     print(sum(anssize))
-
